d7460204d3bcc1b91edc11fe1777802b752da2b3
lucas correia <lucas.correia010103@gmail.com>
2025-07-13 17:12:20 -0300

    chore: project base creation

---
+++ finance-control-api/src/test/java/com/lucascsilva/finance_control_api/FinanceControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucascsilva.finance_control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
