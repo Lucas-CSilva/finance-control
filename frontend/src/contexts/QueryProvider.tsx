e4e4a780eb542250d33480339677d8a7d11f8233
lucas correia <lucas.correia010103@gmail.com>
2025-07-19 21:44:17 -0300

    feature: add QueryProvider context for React Query integration

---
+++ frontend/src/contexts/QueryProvider.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React, { useState } from 'react';
+import {toast} from 'sonner';
+
+interface QueryProviderProps {
+    children: React.ReactNode;
+}
+
+export default function QueryProvier({ children } :  QueryProviderProps) {
+  const [queryClient] = useState(new QueryClient({
+    defaultOptions: {
+      mutations: {
+        onError: (error) => {
+          toast.error(error.message);
+        },
+      },
+    }
+  }));
+
+  return ( 
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
