222c18f861cb6c4c7c003d86d7daaf407b073de4
lucas correia <lucas.correia010103@gmail.com>
2025-07-26 23:25:14 -0300

    feat: add MongoDB configuration with reactive auditing support

---
+++ finance-control-api/src/main/java/com/lucascsilva/finance_control_api/infrastructure/configurations/MongoConfig.java
@@ -0,0 +1,8 @@
+package com.lucascsilva.finance_control_api.infrastructure.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
+
+@Configuration
+@EnableReactiveMongoAuditing
+public class MongoConfig {}
