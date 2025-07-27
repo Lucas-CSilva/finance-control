a952fc6debb34551418cd930af3f05f8f3959b07
lucas correia <lucas.correia010103@gmail.com>
2025-07-26 23:19:45 -0300

    style: format code for consistency in FinanceControlApiApplication and tests

--- finance-control-api/src/main/java/com/lucascsilva/finance_control_api/FinanceControlApiApplication.java
+++ finance-control-api/src/main/java/com/lucascsilva/finance_control_api/FinanceControlApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FinanceControlApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinanceControlApiApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(FinanceControlApiApplication.class, args);
+  }
 }
